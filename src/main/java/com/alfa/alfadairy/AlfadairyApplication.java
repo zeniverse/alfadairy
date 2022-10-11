3a61bacd82a88fbe648e5b08f133bc436eb385de
Unknown <0219alice@gmail.com>
2022-10-11 21:16:54 +0900

    #1 - 프로젝트 생성 후 첫 commit
    프로젝트 생성 후 첫 commit으로 gitignore 파일을 수정해줬음

---
+++ src/main/java/com/alfa/alfadairy/AlfadairyApplication.java
@@ -0,0 +1,13 @@
+package com.alfa.alfadairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlfadairyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlfadairyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alfa/alfadairy/AlfadairyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alfa.alfadairy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlfadairyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
