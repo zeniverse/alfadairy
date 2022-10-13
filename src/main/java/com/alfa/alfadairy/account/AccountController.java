496428d9db693b70dab16a4a98b55d7e9507cdaf
Unknown <0219alice@gmail.com>
2022-10-13 20:04:53 +0900

    #6 - 회원 가입 처리 구현
    sign-up 페이지에서 받아온 SignUpForm 데이터를 가지고 회원 가입 처리를 완료해준다.
    
    - 회원 가입 완료 + 이메일 토큰 발행은 AccountService에서 진행하도록 해준다.
    - 비밀번호는 다음에 Encoding 해줘야한다.

---
+++ src/main/java/com/alfa/alfadairy/account/AccountController.java
@@ -0,0 +1,43 @@
+package com.alfa.alfadairy.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class AccountController {
+
+    private final SignUpFormValidator signUpFormValidator;
+    private final AccountService accountService;
+
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder){
+        webDataBinder.addValidators(signUpFormValidator);
+    }
+
+    @GetMapping("/sign-up")
+    public String signUpForm(Model model){
+        model.addAttribute(new SignUpForm());
+        return "account/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors){
+        if (errors.hasErrors()){
+            return "account/sign-up";
+        }
+
+        accountService.processNewAccount(signUpForm);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/alfa/alfadairy/account/AccountService.java
@@ -0,0 +1,44 @@
+package com.alfa.alfadairy.account;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+    private final JavaMailSender javaMailSender;
+
+
+    public void processNewAccount(SignUpForm signUpForm) {
+        Account newAccount = saveNewAccount(signUpForm);
+        newAccount.generateEmailCheckToken();
+        sendSignUpConfirmEmail(newAccount);
+    }
+
+    private Account saveNewAccount(SignUpForm signUpForm) {
+        Account account = Account.builder()
+                .userId(signUpForm.getUserId())
+                .email(signUpForm.getEmail())
+                .password(signUpForm.getPassword())
+                .build();
+
+        Account newAccount = accountRepository.save(account);
+        return newAccount;
+    }
+
+    private void sendSignUpConfirmEmail(Account newAccount) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(newAccount.getEmail());
+        mailMessage.setSubject("알파축산, 회원 가입 인증 ");
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken() +"&email=" + newAccount.getEmail());
+
+        javaMailSender.send(mailMessage);
+    }
+}
