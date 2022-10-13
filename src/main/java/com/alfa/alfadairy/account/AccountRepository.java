ab7b0117f0bedf6577d3c1ee2bca6adffda8e2fd
Unknown <0219alice@gmail.com>
2022-10-13 20:02:56 +0900

    #6 - AccountRepository 만들기
    회원 가입 할 때, 중복된 아이디 or 이메일인지 확인하기 위해서는 DB속 데이터를 봐야하기 때문에 AccountRepository를 생성해준다.

---
+++ src/main/java/com/alfa/alfadairy/account/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.alfa.alfadairy.account;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByUserId(String userId);
+
+    boolean existsByEmail(String email);
+
+    Account findByEmail(String email);
+}
